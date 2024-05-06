32a6667143f377889435b6f86f3bab70e0154a07
ste <mrbarrow@hotmail.co.uk>
2024-05-06 13:29:35 +0100

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        CheeseRace cr = new CheeseRace();
 
+        String result = cr.race("---M-----K---C--", 6, 6);
+        System.out.println(result);
     }
 }
